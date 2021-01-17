d40f45ac62457ce5933842db2faeb36d7653b11e
unknown <peters0470@gmail.com>
2021-01-17 08:45:37 -0600

    added helpers and auth files to utils folder and set up file structure and starter code for controllers

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));app.engine('handlebars',
 app.set('view engine', 'handlebars');
 app.use(session(sess));
 
-// turn on routes
+
 app.use(routes);
 
 // turn on connection to db and server
